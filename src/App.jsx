ceda04bc6db59a00570e5f5235b426a7365a8bf6
yamaoka-eng <554209835@qq.com>
2022-03-30 09:41:30 +0800

    所有功能完成

--- src/App.jsx
+++ src/App.jsx
@@ -21,22 +21,24 @@ const LoginCard = ({ time }) => {
       <div className={`slide-container
         ${sign ? 'md:translate-x-full md:translate-y-0 translate-y-full' : 'md:translate-x-0 md:translate-y-0 translate-y-0'}
       `}>
-        <div className={`form z-10 opacity-0 transition-all duration-500 ${sign && 'logn-show'}`}>
+        <form className={`slide-container-form transition-all duration-500
+          ${sign ? 'logn-show z-20 opacity-1' : 'z-0 opacity-0'}
+        `}>
             <h1 className='logn-h1'>创建你的账户</h1>
             <span>邮箱可选</span>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>注册</button>
-        </div>
-        <div className='form z-0'>
+            <input className='slide-ct-form-input' type="text" placeholder="Name" />
+            <input className='slide-ct-form-input' type="email" placeholder="Email" />
+            <input className='slide-ct-form-input' type="password" placeholder="Password" />
+            <div className='form-btn'>注册</div>
+        </form>
+        <form className='slide-container-form z-10'>
           <h1 className='logn-h1'>Sign UP</h1>
           <span>请输入您的账户</span>
-          <input type="username" placeholder="Username" />
-          <input type="password" placeholder="Password" />
+          <input className='slide-ct-form-input' type="username" placeholder="Username" />
+          <input className='slide-ct-form-input' type="password" placeholder="Password" />
           <a href="#">忘记密码?</a>
-          <button>登录</button>
-        </div>
+          <div className='form-btn'>登录</div>
+        </form>
       </div>
       <div className={`slide-container z-20
         ${sign ? 'md:translate-x-0 md:translate-y-0 translate-y-0' : 'md:translate-x-full md:translate-y-0 translate-y-full'}
